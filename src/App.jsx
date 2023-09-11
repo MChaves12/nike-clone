6c8b7f10ed370f64e604be40852263cb011a3485
MATHEUS CHAVES ARAUJO <002919@CESED.LOCAL>
2023-09-11 11:56:53 -0300

    Footer section implemented

--- src/App.jsx
+++ src/App.jsx
@@ -33,8 +33,8 @@ const App = () => (
       <CustomerReviews />
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      <Subscribe />
-    </section>/
+      <Subscribe />   
+    </section>
     <section className="bg-black padding-x padding-t pb-8">
       <Footer />
     </section>
